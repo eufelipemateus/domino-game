5559d4e19e16449c34cccfc06798480dfa540c03
Felipe Mateus <eu@felipemateus.com>
2022-03-11 00:16:12 -0300

    feat: deploy updater config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,26 @@
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
   apps : [{
     name   : "domino-game",
     script : "./._build/Server.js",
     watch: true,
+  }],
     deploy : {
         production : {
-                "user" : "root",
-                // "host" : ["192.168.0.13", "192.168.0.14", "192.168.0.15"],
+                "pem": "/home/felipemateus/Documentos/eu.pub",
+                "host": process.env.SERVER_IP,
+                "user": process.env.SERVER_USER,
                 "ref"  : "origin/master",
                 "repo" : "git@github.com:eufelipemateus/domino-game.git",
                 "path" : "/var/www/domino.felipefm32.com",
-                "post-deploy" : "npm install"
+                "post-deploy" : "npm install && pm2 reload ecosystem.config.js  && pm2 save"
           }
     }
 
-  }]
+
+
+
 }
 
